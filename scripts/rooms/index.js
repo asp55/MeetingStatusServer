d66b4d4834230cf649c1e8f03740a65f04268fcf
Andrew Parnell <aparnell@gmail.com>
2021-01-13 22:33:41 -0800

    Moved rooms into a class so that the variable always gets updated

--- scripts/rooms/index.js
+++ scripts/rooms/index.js
@@ -2,47 +2,54 @@ const fs = require('fs');
 const uuid = require("uuid");
 const util = require('util');
 
+
 const roomsFile = "scripts/rooms/rooms.json";
-let rooms = JSON.parse(fs.readFileSync(roomsFile));
-let keys = Object.keys(rooms);
 
-const roomWatcher = fs.watch(roomsFile);
-roomWatcher.on("change", (e)=>{
-    console.log("Update Rooms");
-    rooms = JSON.parse(fs.readFileSync(roomsFile));
-    keys = Object.keys(rooms);
-    
-});
+class Rooms {
+    constructor() {
+        const loadVals = ()=>{
+            this.rooms = JSON.parse(fs.readFileSync(roomsFile));
+            this.keys = Object.keys(this.rooms);
+        }
 
-module.exports = {
-    room: rooms,
-    keys: keys,
-    watch: roomWatcher,
-    name: function(key) {
-        return rooms[key];
-    },
-    add: function (name) {
+        loadVals();
+
+        this.watch = fs.watch(roomsFile);
+        this.watch.on("change", (e)=>{
+            console.log("Update Rooms");
+            loadVals();            
+        });
+    }
+
+    add(name) {
         const newkey = uuid.v5(name, "36873bda-ad21-4655-92fd-c71be19a4882");
-        if(!keys.includes(newkey)) {
-            rooms[newkey] = name;
-            keys.push(newkey);
-            fs.writeFileSync(roomsFile, JSON.stringify(rooms, null, 2));
+        if(!this.keys.includes(newkey)) {
+            this.rooms[newkey] = name;
+            this.keys.push(newkey);
+            fs.writeFileSync(roomsFile, JSON.stringify(this.rooms, null, 2));
             return newkey;
         }
         else {
             throw(`A room named "${name}" already exists`);
         }
-    },
-    remove: function(key) {
-      if(keys.includes(key)) {
-        const name = rooms[key];
-        delete rooms[key];
-        keys.splice(keys.indexOf(key),1);
-        fs.writeFileSync(roomsFile, JSON.stringify(rooms, null, 2));
-        return `${key} (${name}) removed`;
-      }
-      else {
-        throw(`${key} does not exist`);
-      }
+    }
+
+    remove(key) {
+        if(this.keys.includes(key)) {
+            const name = this.rooms[key];
+            delete this.rooms[key];
+            this.keys.splice(this.keys.indexOf(key),1);
+            fs.writeFileSync(roomsFile, JSON.stringify(this.rooms, null, 2));
+            return `${key} (${name}) removed`;
+        }
+        else {
+            throw(`${key} does not exist`);
+        }
+    }
+
+    name(key) {
+        return this.rooms[key];
     }
 }
+
+module.exports = new Rooms();
